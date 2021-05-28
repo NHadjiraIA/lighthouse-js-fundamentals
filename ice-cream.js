7e745ca0664902e293a978ee0607937ef91a5f2f
Hadjira <bkh.hadjira@gmail.com>
2021-05-28 18:48:32 +0000

    add exercise files

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+ 
+console.log(iceCreamFlavours[0]);
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+let x = iceCreamFlavours.length;
+console.log(iceCreamFlavours[x - 1]);
+console.log(x);
